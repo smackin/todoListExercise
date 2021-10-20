86562ccdb107dac547747ca838ba377b523fdfce
Stephanie <smmackin12@gmail.com>
2021-10-20 15:03:26 -0400

    deleted files

--- 1todo2.js
+++
@@ -1,9 +0,0 @@
-const form = document.querySelector('#form');
-const textInput = document.querySelector('input[name="text-input"]');
-
-
-form.addEventListener('submit', function(event){
-    event.preventDefault();
-    alert('it works!');
-});
-
